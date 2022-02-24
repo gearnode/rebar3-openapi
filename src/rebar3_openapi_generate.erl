f5c99c172f3c84b13a8e87b4d2f85856d2385939
gearnode <bryan.frimin@exograd.com>
2022-02-24 14:53:03 +0100

    Debug

--- src/rebar3_openapi_generate.erl
+++ src/rebar3_openapi_generate.erl
@@ -35,7 +35,7 @@ init(State) ->
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
-  rebase_state:get(State, openapi, []),
+  io:format("XXX ~p~n", [State]),
   {ok, State}.
 
 -spec format_error(any()) -> iolist().
